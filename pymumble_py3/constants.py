ac3b6dee0c35841b6513bce740c2fe47d70673c4
azlux <github@azlux.fr>
2021-05-07 23:51:08 +0200

    minor version

--- pymumble_py3/constants.py
+++ pymumble_py3/constants.py
@@ -3,7 +3,7 @@
 import platform
 import sys
 
-PYMUMBLE_VERSION = "1.6"
+PYMUMBLE_VERSION = "1.6.1"
 
 # ============================================================================
 # Tunable parameters
